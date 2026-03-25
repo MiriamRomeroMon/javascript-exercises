7d48fb97ae59d46413e74a43f3779db4bb6c7cb0
Miriam Romero <miriromero15@gmail.com>
2026-03-25 18:24:22 +0100

    Test passed correctly

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let arr = str.split("").reverse().join("");
+    return arr;
 };
+
+reverseString("Hello");
 
 // Do not edit below this line
 module.exports = reverseString;
