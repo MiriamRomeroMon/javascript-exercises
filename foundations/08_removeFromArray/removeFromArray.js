a519db68d61e0ff025de16aa364ef3854b35c55d
Miriam Romero <miriromero15@gmail.com>
2026-03-25 19:13:41 +0100

    Test passed correctly

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...values) {
+    return arr.filter(item=> !values.includes(item));
 };
 
+removeFromArray([1, 2, 3, 4], 3); // should remove 3 and return [1,2,4]
 // Do not edit below this line
 module.exports = removeFromArray;
