1ef24921e712799bd455fef114fc4b01ef67ca76
Miriam Romero <miriromero15@gmail.com>
2026-03-25 18:15:51 +0100

    Test passed correctly

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
-
+  
+const repeatString = function(str, number) {
+    let result ="";
+    if (number < 0) {
+        return "ERROR";
+    }
+    
+    for(let i = 1; i <= number; i++) {
+         result += str;
+    } 
+    
+    return result;
 };
+
+repeatString("hey", 3);
 
 // Do not edit below this line
 module.exports = repeatString;
