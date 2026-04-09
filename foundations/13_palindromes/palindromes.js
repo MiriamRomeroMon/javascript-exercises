bd6af8fb296b8a537f9570c0fe495cca7c648b04
Miriam Romero <miriromero15@gmail.com>
2026-04-09 18:05:09 +0200

    All test passed

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
-
-};
+const palindromes = function (str) {
+    str = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+    let reverse = str.split("").reverse().join("");
+    if (str === reverse) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 // Do not edit below this line
 module.exports = palindromes;
