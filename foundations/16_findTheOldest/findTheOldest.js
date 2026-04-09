4a5270a65b8c907db5dc3d97f1a90d0021b6964d
Miriam Romero <miriromero15@gmail.com>
2026-04-09 19:00:16 +0200

    All tests passed

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const currentDate = new Date().getFullYear();
 
+    const old = people.reduce((oldestPerson, person) => {
+        
+        let oldestAge = (oldestPerson.yearOfDeath || currentDate) - oldestPerson.yearOfBirth;
+        let currentAge = (person.yearOfDeath || currentDate) - person.yearOfBirth;
+
+        if(currentAge > oldestAge) {
+            oldestPerson = person;
+        }
+        return oldestPerson;
+    })
+
+    return old;
 };
 
 // Do not edit below this line
